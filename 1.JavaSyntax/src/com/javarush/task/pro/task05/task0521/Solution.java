b9f24fdc53103fd6c60a46b0964e71df0beadbc8
Timur <timk01@mail.ru>
2023-01-11 23:17:20 +0300

    test 5 lesson

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0521/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0521/Solution.java
@@ -6,11 +6,16 @@ package com.javarush.task.pro.task05.task0521;
 
 public class Solution {
 
-    public static int result;
+    public static int result = 45967;
 
     public static void main(String[] args) {
         String loop = "    for (double fahrenheit = -459.67; fahrenheit < 451; fahrenheit += 40) {\n";
         System.out.println(loop);
     }
 }
-
+//loop.replaceAll("\\W", "").split("fahrenheit")[1]
+// \w, \W: ANY ONE word/non-word character. For ASCII, word characters are [a-zA-Z0-9_]
+// т.е. fordoublefahrenheit45967fahrenheit451fahrenheit40 ((выкинет пробелы, знаки, точки, спецзнаки)) - останутся только слова и цифры
+// loop.replaceAll("\\w", "") -->      (  = -.;  < ;  += ) { ((все цифры, буквы))
+//"fordoublefahrenheit45967fahrenheit451fahrenheit40".split("fahrenheit") разсплитит на массив ["fordouble", "45967", "451", "40"]
+//[] возьмет его 2 элемент (по индексу 1)
